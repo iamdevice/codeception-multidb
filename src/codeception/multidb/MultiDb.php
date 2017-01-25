4ac803be7be6b3a1330bba2dfb60bf1179944948
Aleksandr Kozhevnikov <iamdevice@gmail.com>
2017-01-25 21:41:15 +0400

    first commit

---
+++ src/codeception/multidb/MultiDb.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Codeception\Exception;
+
+use Codeception\Lib\Driver\Db;
+use Codeception\Module;
+
+class MultiDb extends Module
+{
+    /**
+     * @var array
+     */
+    protected $connections = [];
+
+    /**
+     * @var Db[]
+     */
+    protected $drivers = [];
+
+    protected $config = [
+        'connectors' => null
+    ];
+
+    /**
+     * @var array
+     */
+    protected $insertedRows = [];
+
+    protected $requiredFields = ['connections'];
+
+    protected $connectionRequiredFields = ['dsn', 'user', 'password'];
+
+    public function _initialize()
+    {
+        if (is_array($this->config['connectors'])) {
+            print_r($this->config['connectors']);
+        }
+    }
+
+    private function readSql()
+    {
+
+    }
+}
